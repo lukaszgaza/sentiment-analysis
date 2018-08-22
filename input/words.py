c762f30db3dc9de35f47d354bc455dd652b39ccb
Lukasz Gaza <lgaza@virtuslab.com>
2018-08-22 12:52:09 +0200

    Added reading and printing scored words

--- input/words.py
+++ input/words.py
@@ -1,7 +1,16 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 class ScoredWords:
 
     def __init__(self, input_file_path):
         self.words = pd.read_csv(input_file_path)
+
+    def words_df(self):
+        return self.words
+
+    def hist(self):
+        plt.hist(self.words["Average"], bins=10)
+        plt.xlabel("Sentiment")
+        plt.ylabel("# of words")
+        plt.show()

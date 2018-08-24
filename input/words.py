789038407b4d121268eb7de81096a5e806c13cb9
Lukasz Gaza <lgaza@virtuslab.com>
2018-08-24 11:55:51 +0200

    Added method to return list of scored words

--- input/words.py
+++ input/words.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class ScoredWords:
 
     def __init__(self, input_file_path):
         self.words = pd.read_csv(input_file_path)
 
-    def words_df(self):
+    def get_words_df(self):
         return self.words
+
+    def get_words(self):
+        return [word for word in self.words['Word']]
 
     def hist(self):
         plt.hist(self.words["Average"], bins=10)

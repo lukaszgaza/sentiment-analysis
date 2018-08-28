4d63215867380a8115603e4a17dcdaacc14ca21b
Lukasz Gaza <lgaza@virtuslab.com>
2018-08-28 16:27:16 +0200

    SA-6 Distance to neutral sentiment analysis

--- analysis/distance/distance_analysis.py
+++ analysis/distance/distance_analysis.py
@@ -26,3 +26,27 @@ class DistanceToPreviousWordWithSentimentAnalysis(analysis.general_analysis.Gene
                 prev_count += 1
 
         return self.scores
+
+
+class DistanceToNeutralSentimentAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'distance_to_neutral_sentiment'
+    NEUTRAL_SENTIMENT = 5.0
+
+    def __init__(self, book):
+        super().__init__(DistanceToPreviousWordWithSentimentAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Distance to neutral (5.0) sentiment sentiment'
+        self.plot_xlabel = 'Word number'
+        self.plot_ylabel = 'Distance'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        self.scores = []
+
+        scores_by_words = scored_input_words_df.get_scores_by_words()
+        for word in words:
+            if word in scores_by_words:
+                score = scores_by_words[word]
+                self.scores.append(score - DistanceToNeutralSentimentAnalysis.NEUTRAL_SENTIMENT)
+
+        return self.scores

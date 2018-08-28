e9601d967538efd3bcc31f9564d8524868ab0ebd
Lukasz Gaza <lgaza@virtuslab.com>
2018-08-28 11:31:29 +0200

    SA-7 Added authors and titles to the books

--- input/file_input.py
+++ input/file_input.py
@@ -5,10 +5,10 @@ from nltk import word_tokenize, sent_tokenize
 
 class FileInput(general_input.Input):
 
-    def __init__(self, path, title):
+    def __init__(self, path, author, title):
         self.path = path
+        self.author = author
         self.title = title
-        self.author = ''
         self.raw = io.open(path, mode="r", encoding="utf-8").read()
 
     def get_raw_text(self):
--- input/gutenberg_input.py
+++ input/gutenberg_input.py
@@ -5,10 +5,10 @@ from nltk import word_tokenize
 
 class GutenbergInput(general_input.Input):
 
-    def __init__(self, gutenberg_name, title):
+    def __init__(self, gutenberg_name, author, title):
         self.gutenberg_name = gutenberg_name
+        self.author = author
         self.title = title
-        self.author = ''
 
     def get_raw_text(self):
         return nltk.corpus.gutenberg.raw(self.gutenberg_name)

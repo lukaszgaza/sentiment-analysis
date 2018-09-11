1a3990bb507d105d98cd4504fe96d1e6597d8544
Lukasz Gaza <lgaza@virtuslab.com>
2018-09-11 14:19:14 +0200

    SA-30 Distance of given word to word with given sentiment

--- analysis/distance/distance_analysis.py
+++ analysis/distance/distance_analysis.py
@@ -1,6 +1,6 @@
 import analysis.general_analysis
 import analysis.scoring
-
+import sys
 
 class DistanceToPreviousWordWithSentimentAnalysis(analysis.general_analysis.GeneralAnalysis):
 
@@ -26,6 +26,100 @@ class DistanceToPreviousWordWithSentimentAnalysis(analysis.general_analysis.Gene
                 prev_count += 1
 
         return self.scores
+
+
+class DistanceToWordWithSentimentHigherThan(analysis.general_analysis.GeneralAnalysis,):
+
+    def __init__(self, book, min_sentiment):
+        ANALYSIS_NAME = 'distance_to_word_with_sentiment_higher_than_{}'.format(min_sentiment)
+        super().__init__(ANALYSIS_NAME, book)
+        self.plot_title = 'Distance to word with sentiment higher than {}'.format(min_sentiment)
+        self.plot_xlabel = 'Word number'
+        self.plot_ylabel = 'Distance'
+        self.min_sentiment = min_sentiment
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        source_words = scored_input_words_df.get_scores_by_words()
+
+        self.scores = []
+        scores_prev = self._score_helper(words, source_words)
+        scores_next = self._score_helper(reversed(words), source_words)
+        scores_next = reversed(scores_next)
+
+        all_scores = [scores_prev, scores_next]
+
+        temp_scores = [min(column) for column in zip(*all_scores)]
+
+        for pos in range(len(words)):
+            self.scores.append(temp_scores[pos])
+
+        return self.scores
+
+    def _score_helper(self, words, source_words):
+        prev_count = sys.maxsize
+        scores = []
+        for word in words:
+            if word in source_words:
+                score = source_words[word]
+            else:
+                score = 0
+            if score > self.min_sentiment:
+                prev_count = 0
+            else:
+                if prev_count < sys.maxsize:
+                    prev_count += 1
+
+            scores.append(prev_count)
+
+        return scores
+
+
+class DistanceToWordWithModuloSentimentHigherThan(analysis.general_analysis.GeneralAnalysis,):
+
+    def __init__(self, book, min_sentiment):
+        ANALYSIS_NAME = 'distance_to_word_with_modulo_sentiment_higher_than_{}'.format(min_sentiment)
+        super().__init__(ANALYSIS_NAME, book)
+        self.plot_title = 'Distance to word with modulo sentiment higher than {}'.format(min_sentiment)
+        self.plot_xlabel = 'Word number'
+        self.plot_ylabel = 'Distance'
+        self.min_sentiment = min_sentiment
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        source_words = scored_input_words_df.get_scores_by_words()
+
+        self.scores = []
+        scores_prev = self._score_helper(words, source_words)
+        scores_next = self._score_helper(reversed(words), source_words)
+        scores_next = reversed(scores_next)
+
+        all_scores = [scores_prev, scores_next]
+
+        temp_scores = [min(column) for column in zip(*all_scores)]
+
+        for pos in range(len(words)):
+            self.scores.append(temp_scores[pos])
+
+        return self.scores
+
+    def _score_helper(self, words, source_words):
+        prev_count = sys.maxsize
+        scores = []
+        for word in words:
+            if word in source_words:
+                score = abs(source_words[word])
+            else:
+                score = 0
+            if score > self.min_sentiment:
+                prev_count = 0
+            else:
+                if prev_count < sys.maxsize:
+                    prev_count += 1
+
+            scores.append(prev_count)
+
+        return scores
 
 
 class DistanceToNeutralSentimentAnalysis(analysis.general_analysis.GeneralAnalysis):

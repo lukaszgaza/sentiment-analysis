8927f740629914367fe1a38b3618a63254985299
Lukasz Gaza <lgaza@virtuslab.com>
2018-09-11 15:52:01 +0200

    SA-9 Separate analysis for positive and negative words

--- analysis/sentence/sentence_analysis.py
+++ analysis/sentence/sentence_analysis.py
@@ -74,6 +74,114 @@ class SumScoredPerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
         return self.scores
 
 
+class SumScoredByScoredCountOnlyPositivePerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_scored_count_only_positive_per_sentence'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByScoredCountOnlyPositivePerSentenceAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a sentence by count of scored words (only positive)'
+        self.plot_xlabel = 'Sentence number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        sentences = self.book.get_sentences()
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_scored_words(scored_input_words_df, analysis.scoring.POSITIVE_FILTER), sentences))
+
+        return self.scores
+
+
+class SumScoredByAllCountOnlyPositivePerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_all_count_only_positive_per_sentence'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByAllCountOnlyPositivePerSentenceAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a sentence by count of all words (only positive)'
+        self.plot_xlabel = 'Sentence number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        sentences = self.book.get_sentences()
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_all_words(scored_input_words_df, analysis.scoring.POSITIVE_FILTER), sentences))
+
+        return self.scores
+
+
+class SumScoredOnlyPositivePerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_per_only_positive_sentence'
+
+    def __init__(self, book):
+        super().__init__(SumScoredOnlyPositivePerSentenceAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a sentence (only positive)'
+        self.plot_xlabel = 'Sentence number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        sentences = self.book.get_sentences()
+
+        self.scores = list(map(analysis.scoring.score_slice_as_sum_of_all_words(scored_input_words_df, analysis.scoring.POSITIVE_FILTER), sentences))
+
+        return self.scores
+
+
+class SumScoredByScoredCountOnlyNegativePerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_scored_count_only_negative_per_sentence'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByScoredCountOnlyNegativePerSentenceAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a sentence by count of scored words (only negative)'
+        self.plot_xlabel = 'Sentence number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        sentences = self.book.get_sentences()
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_scored_words(scored_input_words_df, analysis.scoring.NEGATIVE_FILTER), sentences))
+
+        return self.scores
+
+
+class SumScoredByAllCountOnlyNegativePerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_all_count_only_negative_per_sentence'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByAllCountOnlyNegativePerSentenceAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a sentence by count of all words (only negative)'
+        self.plot_xlabel = 'Sentence number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        sentences = self.book.get_sentences()
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_all_words(scored_input_words_df, analysis.scoring.NEGATIVE_FILTER), sentences))
+
+        return self.scores
+
+
+class SumScoredOnlyNegativePerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_only_negative_per_sentence'
+
+    def __init__(self, book):
+        super().__init__(SumScoredOnlyNegativePerSentenceAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a sentence (only negative)'
+        self.plot_xlabel = 'Sentence number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        sentences = self.book.get_sentences()
+
+        self.scores = list(map(analysis.scoring.score_slice_as_sum_of_all_words(scored_input_words_df, analysis.scoring.NEGATIVE_FILTER), sentences))
+
+        return self.scores
+
+
 class NumberOfWordsWithSentimentPerSentenceAnalysis(analysis.general_analysis.GeneralAnalysis):
 
     ANALYSIS_NAME = 'number_of_words_with_sentiment_per_sentence'
--- analysis/slice/slice_analysis.py
+++ analysis/slice/slice_analysis.py
@@ -79,6 +79,120 @@ class SumScoredAnalysis(analysis.general_analysis.GeneralAnalysis):
         return self.scores
 
 
+class SumScoredByScoredCountOnlyPositiveAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_scored_count_only_positive'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByScoredCountOnlyPositiveAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a slice by count of scored words (only positive)'
+        self.plot_xlabel = 'Slice number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        slices = analysis.slice_generator.SliceGenerator(slice_size).get_non_overlapping_windows(words)
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_scored_words(scored_input_words_df, analysis.scoring.POSITIVE_FILTER), slices))
+
+        return self.scores
+
+
+class SumScoredByAllCountOnlyPositiveAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_all_count_only_positive'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByAllCountOnlyPositiveAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a slice by count of all words (only positive)'
+        self.plot_xlabel = 'Slice number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        slices = analysis.slice_generator.SliceGenerator(slice_size).get_non_overlapping_windows(words)
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_all_words(scored_input_words_df, analysis.scoring.POSITIVE_FILTER), slices))
+
+        return self.scores
+
+
+class SumScoredOnlyPositiveAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_only_positive'
+
+    def __init__(self, book):
+        super().__init__(SumScoredOnlyPositiveAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a slice (only positive)'
+        self.plot_xlabel = 'Slice number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        slices = analysis.slice_generator.SliceGenerator(slice_size).get_non_overlapping_windows(words)
+
+        self.scores = list(map(analysis.scoring.score_slice_as_sum_of_all_words(scored_input_words_df, analysis.scoring.POSITIVE_FILTER), slices))
+
+        return self.scores
+
+
+class SumScoredByScoredCountOnlyNegativeAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_scored_count_only_negative'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByScoredCountOnlyNegativeAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a slice by count of scored words (only negative)'
+        self.plot_xlabel = 'Slice number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        slices = analysis.slice_generator.SliceGenerator(slice_size).get_non_overlapping_windows(words)
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_scored_words(scored_input_words_df, analysis.scoring.NEGATIVE_FILTER), slices))
+
+        return self.scores
+
+
+class SumScoredByAllCountOnlyNegativeAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_by_all_count_only_negative'
+
+    def __init__(self, book):
+        super().__init__(SumScoredByAllCountOnlyNegativeAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a slice by count of all words (only negative)'
+        self.plot_xlabel = 'Slice number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        slices = analysis.slice_generator.SliceGenerator(slice_size).get_non_overlapping_windows(words)
+
+        self.scores = list(map(analysis.scoring.score_slice_as_avg_of_all_words(scored_input_words_df, analysis.scoring.NEGATIVE_FILTER), slices))
+
+        return self.scores
+
+
+class SumScoredOnlyNegativeAnalysis(analysis.general_analysis.GeneralAnalysis):
+
+    ANALYSIS_NAME = 'sum_scored_only_negative'
+
+    def __init__(self, book):
+        super().__init__(SumScoredOnlyNegativeAnalysis.ANALYSIS_NAME, book)
+        self.plot_title = 'Sentiment as sum of scored words in a slice (only negative)'
+        self.plot_xlabel = 'Slice number'
+        self.plot_ylabel = 'Sentiment'
+
+    def score(self, slice_size, scored_input_words_df):
+        words = self.book.get_words_lower()
+        slices = analysis.slice_generator.SliceGenerator(slice_size).get_non_overlapping_windows(words)
+
+        self.scores = list(map(analysis.scoring.score_slice_as_sum_of_all_words(scored_input_words_df, analysis.scoring.NEGATIVE_FILTER), slices))
+
+        return self.scores
+
+
 class NumberOfWordsWithSentimentPerSliceAnalysis(analysis.general_analysis.GeneralAnalysis):
 
     ANALYSIS_NAME = 'number_of_words_with_sentiment_per_slice'
